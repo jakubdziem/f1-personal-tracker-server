a66d906b35567c1917404769eea77572bbb47b3e
Jakub Dziem <jakub.dziem9@gmail.com>
2025-05-18 10:55:01 +0200

    Init commit

---
+++ src/main/java/com/dziem/f1_personal_tracker/F1PersonalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dziem.f1_personal_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1PersonalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1PersonalTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dziem/f1_personal_tracker/F1PersonalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dziem.f1_personal_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1PersonalTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
