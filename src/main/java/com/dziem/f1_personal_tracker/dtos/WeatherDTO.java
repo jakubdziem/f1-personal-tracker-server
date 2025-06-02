f37045c429609b4898f6e23cab8b36f50588515e
Jakub Dziem <jakub.dziem9@gmail.com>
2025-06-02 23:07:22 +0200

    fix: -weatherDTO now doesn't have id

--- src/main/java/com/dziem/f1_personal_tracker/dtos/WeatherDTO.java
+++ src/main/java/com/dziem/f1_personal_tracker/dtos/WeatherDTO.java
@@ -1,11 +1,13 @@
 package com.dziem.f1_personal_tracker.dtos;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
 @Data
+@Builder
 public class WeatherDTO {
-    Integer id;
     LocalDate time;
     RaceDTO raceDTO;
     Integer rainfall;
