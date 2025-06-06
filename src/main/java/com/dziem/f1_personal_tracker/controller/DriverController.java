ccc73d24c4420683bde8db6d37303da93ddec81f
Jakub Dziem <jakub.dziem9@gmail.com>
2025-06-07 00:37:57 +0200

    add: added endpoints for frontend

---
+++ src/main/java/com/dziem/f1_personal_tracker/controller/DriverController.java
@@ -0,0 +1,27 @@
+package com.dziem.f1_personal_tracker.controller;
+
+import com.dziem.f1_personal_tracker.dtos.DriverAveragePositionDTO;
+import com.dziem.f1_personal_tracker.dtos.TemperatureLapTimeDTO;
+import com.dziem.f1_personal_tracker.service.DriverAveragePositionService;
+import com.dziem.f1_personal_tracker.service.TemperatureLapTimeService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/api/v1")
+@AllArgsConstructor
+public class DriverController {
+    private final DriverAveragePositionService driverAveragePositionService;
+    private final TemperatureLapTimeService temperatureLapTimeService;
+
+    @GetMapping("/drivers")
+    public List<DriverAveragePositionDTO> getDrivers() {
+        return driverAveragePositionService.getDrivers();
+    }
+    @GetMapping("/weather")
+    public List<TemperatureLapTimeDTO> getTemperatureLapTimeDTOS() {
+        return temperatureLapTimeService.getTemperatureLapTimeDTOs();
+    }
+}
