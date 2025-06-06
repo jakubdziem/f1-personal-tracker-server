80eda1afed54e2e1afe4ea66c440b19070b5740c
Jakub Dziem <jakub.dziem9@gmail.com>
2025-06-07 00:38:33 +0200

    add: LapTimeRepository and methods for RaceRepository

--- src/main/java/com/dziem/f1_personal_tracker/repository/RaceRepository.java
+++ src/main/java/com/dziem/f1_personal_tracker/repository/RaceRepository.java
@@ -2,6 +2,13 @@ package com.dziem.f1_personal_tracker.repository;
 
 import com.dziem.f1_personal_tracker.model.Race;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface RaceRepository extends JpaRepository<Race, Integer> {
+    Optional<List<Race>> findRacesByName(String name);
+    @Query("SELECT DISTINCT r FROM Race r LEFT JOIN FETCH r.weatherList")
+    List<Race> findAllWithWeather();
 }
