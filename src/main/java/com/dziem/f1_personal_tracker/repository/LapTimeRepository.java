98968079df17b48d14f9ff884fc6573103bcbbf5
Jakub Dziem <jakub.dziem9@gmail.com>
2025-06-07 09:49:45 +0200

    add: custom queries in repositories

--- src/main/java/com/dziem/f1_personal_tracker/repository/LapTimeRepository.java
+++ src/main/java/com/dziem/f1_personal_tracker/repository/LapTimeRepository.java
@@ -1,7 +1,11 @@
 package com.dziem.f1_personal_tracker.repository;
 
+import com.dziem.f1_personal_tracker.model.Driver;
 import com.dziem.f1_personal_tracker.model.LapTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LapTimeRepository extends JpaRepository<LapTime, Integer> {
+    List<LapTime> findAllByDriver(Driver driver);
 }
--- src/main/java/com/dziem/f1_personal_tracker/repository/ResultRepository.java
+++ src/main/java/com/dziem/f1_personal_tracker/repository/ResultRepository.java
@@ -1,7 +1,16 @@
 package com.dziem.f1_personal_tracker.repository;
 
+import com.dziem.f1_personal_tracker.model.Driver;
 import com.dziem.f1_personal_tracker.model.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ResultRepository extends JpaRepository<Result, Integer> {
+    @Query("SELECT r FROM Result r WHERE r.race.year IN :years")
+    List<Result> findByRaceYearIn(@Param("years") List<java.time.Year> years);
+
+    List<Result> findAllByDriver(Driver driver);
 }
